1695a5fde6c7c5e6a9efd807c8d1942daf31952d
Scott <spacevtime@users.noreply.github.com>
2015-05-08 18:37:25 -0700

    Update config.js
    Updated for Los Angeles, CA and NYT news feed.

--- js/config.js
+++ js/config.js
@@ -6,15 +6,15 @@ var lang = window.navigator.language;
 //change weather params here:
 //units: metric or imperial
 var weatherParams = {
-    'q':'Baarn,Netherlands',
-    'units':'metric',
+    'q':'Los+Angeles,US',
+    'units':'imperial',
     'lang':lang
 };
 
-var feed = 'http://feeds.nos.nl/nosjournaal?format=rss';
+//var feed = 'http://feeds.nos.nl/nosjournaal?format=rss';
 //var feed = 'http://www.nu.nl/feeds/rss/achterklap.rss';
 //var feed = 'http://www.nu.nl/feeds/rss/opmerkelijk.rss';
-//var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
 
 // compliments:
 var morning = [
